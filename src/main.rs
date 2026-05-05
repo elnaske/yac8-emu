42eb6ebe20ac7c3b9f156f7b48512f83b5d1db60
elnaske <naske.elias@outlook.com>
2026-05-05 11:51:07 -0400

    Add screen buffer drawing

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,22 @@ const PIXEL_SIZE: u32 = 10;
 const SCREEN_WIDTH: u32 = 64 * PIXEL_SIZE;
 const SCREEN_HEIGHT: u32 = 32 * PIXEL_SIZE;
 
+pub struct ScreenBuffer([[bool;64];32]);
+impl ScreenBuffer {
+    pub fn new() -> Self {
+        ScreenBuffer([[false;64];32])
+    }
+}
+
 pub struct Display {
     pub canvas: WindowCanvas,
+    on_color: Color,
+    off_color: Color,
 }
 impl Display {
-    pub fn new(window: Window) -> Result<Self, String> {
+    pub fn new(window: Window, on_color: Color, off_color: Color) -> Result<Self, String> {
         let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-        Ok(Display { canvas })
+        Ok(Display { canvas, on_color, off_color })
     }
 
     pub fn draw_pixel(&mut self, x: i32, y: i32) -> Result<(), String> {
@@ -27,6 +36,20 @@ impl Display {
             PIXEL_SIZE,
             PIXEL_SIZE,
         ))?;
+        Ok(())
+    }
+
+    pub fn draw_screen_buffer(&mut self, buff: ScreenBuffer) -> Result<(), String> {
+        for x in 0..64 {
+            for y in 0..32 {
+                let color = match buff.0[y][x] {
+                    true => self.on_color,
+                    false => self.off_color,
+                };
+                self.canvas.set_draw_color(color);
+                self.draw_pixel(x as i32, y as i32)?;
+            }
+        }
         Ok(())
     }
 }
@@ -42,21 +65,16 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut display = Display::new(window).unwrap();
+    let mut display = Display::new(window, Color::WHITE, Color::BLACK).unwrap();
+    let mut screen = ScreenBuffer::new();
 
     for x in 0..64 {
         for y in 0..32 {
-            let color = {
-                if (x % 2 == 0) ^ (y % 2 == 0) {
-                    Color::BLACK
-                } else {
-                    Color::WHITE
-                }
-            };
-            display.canvas.set_draw_color(color);
-            display.draw_pixel(x, y).unwrap();
+            screen.0[y][x] = (x % 2 == 0) ^ (y % 2 == 0);
         }
     }
+
+    display.draw_screen_buffer(screen).unwrap();
     display.canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
